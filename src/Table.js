73435375274c10b85691ae965bbaaaeeddc74125
at-rooshee <rushania@youcanbook.me>
2019-03-06 00:13:35 -0600

    Adding props

--- src/Table.js
+++ src/Table.js
@@ -1,36 +1,40 @@
 import React, {Component} from 'react';
+const TableHeader = () => {
+    return (
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Job</th>
+            </tr>
+        </thead>
+    );
+}
+
+const TableBody = props => {
+    const rows = props.characterData.map((row, index) => {
+        return (
+            <tr key={index}>
+                <td>{row.name}</td>
+                <td>{row.job}</td>
+            </tr>
+        );
+    });
+
+    return <tbody>{rows}</tbody>;
+}
 
 class Table extends Component {
     render() {
+        const { characterData } = this.props;
+
         return (
             <table>
-                <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th>Job</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>Charlie</td>
-                        <td>Janitor</td>
-                    </tr>
-                    <tr>
-                        <td>Mac</td>
-                        <td>Bouncer</td>
-                    </tr>
-                    <tr>
-                        <td>Dee</td>
-                        <td>Aspiring actress</td>
-                    </tr>
-                    <tr>
-                        <td>Dennis</td>
-                        <td>Bartender</td>
-                    </tr>
-                </tbody>
+                <TableHeader />
+                <TableBody characterData={characterData} />
             </table>
         );
     }
 }
+
 
 export default Table;
